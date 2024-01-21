61c4fb867cfd8177f50a7c904010fda56be5fd6d
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2024-01-21 20:15:26 +0530

    scene management added

--- GameEon Game Jam 2024/Assets/Scripts/GameManagerScript.cs
+++ GameEon Game Jam 2024/Assets/Scripts/GameManagerScript.cs
@@ -6,6 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
+    /*private GameObject player1;
+    private GameObject player2;
+
+    private void Start()
+    {
+        player1 = GetComponent<PlayerSwitch>().player1;
+        player2 = GetComponent<PlayerSwitch>().player2;
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void ResetLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
