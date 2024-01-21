1327ea959ded3347f43125239f89e84243904d1d
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2024-01-21 20:04:02 +0530

    more UI added

--- GameEon Game Jam 2024/Assets/Scripts/GameManagerScript.cs
+++ GameEon Game Jam 2024/Assets/Scripts/GameManagerScript.cs
@@ -2,8 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
-    
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
