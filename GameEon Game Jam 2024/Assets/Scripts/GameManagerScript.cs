0bf68a0ffd429fbaceeb28a592049936cd1bf3d3
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2024-01-21 19:34:21 +0530

    basic UI added

--- GameEon Game Jam 2024/Assets/Scripts/GameManagerScript.cs
+++ GameEon Game Jam 2024/Assets/Scripts/GameManagerScript.cs
@@ -5,30 +5,5 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public Transform respawnPoint;
-
-    private GameObject activePlayer;
-
-    private PlayerSwitch playerSwitch;
-
-    private GameObject player1;
-    private GameObject player2;
-
-    private void Start()
-    {
-        playerSwitch = GetComponent<PlayerSwitch>();
-        player1 = GetComponent<PlayerSwitch>().player1;
-        player2 = GetComponent<PlayerSwitch>().player2;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject == player1 || other.gameObject == player2)
-        {
-            activePlayer = playerSwitch.activePlayer.gameObject;
-            Debug.Log(activePlayer);
-
-            activePlayer.transform.position = respawnPoint.transform.position;
-        }
-    }
+    
 }
---
+++ GameEon Game Jam 2024/Assets/Scripts/RespawnPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+    public Transform respawnPoint;
+
+    private GameObject activePlayer;
+
+    private PlayerSwitch playerSwitch;
+
+    private GameObject player1;
+    private GameObject player2;
+
+    private void Start()
+    {
+        playerSwitch = GetComponent<PlayerSwitch>();
+        player1 = GetComponent<PlayerSwitch>().player1;
+        player2 = GetComponent<PlayerSwitch>().player2;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player1 || other.gameObject == player2)
+        {
+            activePlayer = playerSwitch.activePlayer.gameObject;
+            Debug.Log(activePlayer);
+
+            activePlayer.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
